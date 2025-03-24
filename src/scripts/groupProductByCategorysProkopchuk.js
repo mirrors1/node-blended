60080261dbac325c78d118b4c66f73ffba8af759
Oleksandr Prokopchuk <sasha195@i.ua>
2025-03-24 10:39:49 +0200

    Added comments and optimized the task groupProductByCategorysProkopchuk

--- src/scripts/groupProductByCategorysProkopchuk.js
+++ src/scripts/groupProductByCategorysProkopchuk.js
@@ -5,13 +5,18 @@ let products = [];
 
 const groupProductByCategorysProkopchuk = async () => {
   try {
+    //Зчитуємо дані з файлу
     const productsData = await fs.readFile(PATH_DB, "UTF-8");
+    //Приводимо дані до JSON
     products = JSON.parse(productsData);
+    //Вибираємо в новий масив всі значення з ключа "category"
     const categories = products.map(({ category }) => category);
-    const productsСategories = categories.filter(
-      (item, index, arr) => arr.indexOf(item) === index
-    );
+    //Зберігаємо в колекцію унікальні значення
+    const productsСategories = [...new set(categories)];
+    //Створюємо новий об'єкт по умові ТЗ
     let objectResult = {};
+    //Зберігаємо в об'єкт ключ (значення category)
+    //Значення ключа об'єкту дорівнює масиву назв продуктів, які належать до цієї категорії.
     productsСategories.forEach((element) => {
       objectResult[element] = ProductByCategorys(element);
     });
@@ -21,6 +26,7 @@ const groupProductByCategorysProkopchuk = async () => {
   }
 };
 
+//функція повертає масив назв продуктів по вказаній категорії
 const ProductByCategorys = (categoryName) => {
   const findProducts = products.filter(
     (product) => product.category === categoryName
